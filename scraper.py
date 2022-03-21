53f6bbb1c86a5add4da705667c905354549af716
Ayman <ayman.mezghani@epfl.ch>
2022-03-21 18:48:29 +0100

    add timestamping support

--- scraper.py
+++ scraper.py
@@ -1,53 +1,79 @@
+import numpy as np
+import pandas as pd
 import tweepy
 import json
-from typing import List
+from typing import List, Tuple
 from datetime import datetime
 
 QUERY_CHAR_LIMIT = 1024
 
 
-def _splitter_helper(ls: List[str], char_limit: int) -> List[List[str]]:
+def _splitter_helper(ls: List[Tuple[str, datetime]], char_limit: int,
+                     earliest_start: datetime) -> List[Tuple[List[str], datetime]]:
     # init step
     res = []
     sublist = []
     sublist_char_length = 0
+    sublist_timestamp = datetime.utcnow()
 
-    for kw in ls:
+    for kw, t in ls:
         # 3 to account for ' OR' to be prepended to each string when constructing the query
         entry_length = len(kw) + 3
 
         # if sublist can't contain the new item
         if sublist_char_length + entry_length > char_limit:
-            print(sublist_char_length)
-            res.append(sublist)
+            # print(sublist_char_length)
+            res.append((sublist, sublist_timestamp))
 
             sublist = []
             sublist_char_length = 0
+            sublist_timestamp = datetime.utcnow()
 
-        # append account_id to the sublist and add its corresponding entry length to sublist_char_length
+        # timestamp should be the earliest, while not earlier than `earliest_start`
+        if sublist_timestamp > earliest_start:
+            if t is None or pd.isna(t):
+                sublist_timestamp = earliest_start
+            else:
+                sublist_timestamp = max(min(sublist_timestamp, t), earliest_start)
+
+        # append kw to the sublist and add its corresponding entry length to sublist_char_length
         sublist.append(kw)
         sublist_char_length += entry_length
 
     # final step
-    print(sublist_char_length)
-    res.append(sublist)
+    # print(sublist_char_length)
+    res.append((sublist, sublist_timestamp))
 
     return res
 
 
-def splitter(kw_list: List[str], char_limit: int) -> List[List[str]]:
+def splitter(kw_list: List[Tuple[str, datetime]], char_limit: int,
+             earliest_start: datetime) -> List[Tuple[List[str], str]]:
     """
     Splits a list of accounts into smaller slices of max size `char_limit`.
 
     :param kw_list: keywords list
     :param char_limit: limit of number of characters per slice
+    :param earliest_start: earliest start date for tweet fetching
     :return: a list containing slices of keywords
     """
 
-    res = []
+    none_time_list = [e for e in kw_list if e[1] is None or pd.isna(e[1])]
 
-    if len(kw_list) != 0:
-        res += _splitter_helper(kw_list, char_limit)
+    print('none', len(none_time_list))
+    if len(none_time_list) != 0:
+        res = _splitter_helper(none_time_list, char_limit, earliest_start)
+    else:
+        res = []
+
+    sorted_by_timestamp = sorted([e for e in kw_list if not (e[1] is None or pd.isna(e[1]))],
+                                 key=lambda x: x[1],
+                                 reverse=True)
+
+    print('sorted', len(sorted_by_timestamp))
+
+    if len(sorted_by_timestamp) != 0:
+        res += _splitter_helper(sorted_by_timestamp, char_limit, earliest_start)
 
     return res
 
@@ -88,7 +114,7 @@ def get_tweets(client: tweepy.Client, keywords: List[str], start_time: datetime,
     tweets = client.search_all_tweets(query=query,
                                       start_time=start_time,
                                       end_time=end_time,
-                                      max_results=500,
+                                      max_results=100,
                                       tweet_fields=','.join(
                                           ['id', 'text', 'attachments', 'author_id', 'context_annotations',
                                            'conversation_id', 'created_at', 'entities', 'geo', 'in_reply_to_user_id',
